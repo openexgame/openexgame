7ce55ce8e3aee7322d4cc158e9195a693af20bff
panda <SmelayaPanda@mail.ru>
2018-03-27 11:47:15 +0700

    test first

--- src/filters/snippet.js
+++
@@ -1,9 +0,0 @@
-export default (value, maxLength) => {
-  let val
-  if (value.length > maxLength) {
-    val = value.slice(0, maxLength) + ' . . .'
-  } else {
-    val = value
-  }
-  return val
-}
