9c98a76d29712a72114f3eca1edba5efe3998829
panda <SmelayaPanda@mail.ru>
2018-03-28 03:06:26 +0700

    Revert "global stiles and main page carcase"
    This reverts commit 97c7f47

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <app-header v-if="showCommonHeader"></app-header>
+    <app-header v-if="!this.$router.history.current.fullPath.includes('admin')"></app-header>
     <!--Content-->
     <transition name="fade">
       <router-view></router-view>
@@ -13,43 +13,39 @@ import AppHeader from '@/components/Header'
 import AdminPanel from './components/admin/AdminPanel'
 
 export default {
-  name: 'App',
   components: {
     AppHeader,
     AdminPanel
   },
-  data () {
-    return {}
-  },
-  computed: {
-    showCommonHeader () {
-      return !this.$router.history.current.fullPath.includes('admin') &&
-          this.$router.history.current.fullPath !== '/'
-    }
-  }
+  name: 'App'
 }
 </script>
 
-<style lang="sass">
-  body, html
-    margin: 0
-    padding: 0
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
 
-    #app
-      font-family: Montserrat, 'Avenir', Helvetica, Arial, sans-serif
-      -webkit-font-smoothing: antialiased
-      -moz-osx-font-smoothing: grayscale
-      background-color: white
-      text-align: center
-      color: #2c3e50
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: white;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-    .fade-enter-active, .fade-leave-active
-      transition-property: opacity
-      transition-duration: .25s
+  .fade-enter-active, .fade-leave-active {
+    transition-property: opacity;
+    transition-duration: .25s;
+  }
 
-    .fade-enter-active
-      transition-delay: .25s
+  .fade-enter-active {
+    transition-delay: .25s;
+  }
 
-    .fade-enter, .fade-leave-active
-      opacity: 0
+  .fade-enter, .fade-leave-active {
+    opacity: 0
+  }
 </style>
